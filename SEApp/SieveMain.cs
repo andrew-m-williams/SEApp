a6f9ef3e350896530d5d6b1c948794a8222bbd3c
andrew_sb <andrewmrwilliams@gmail.com>
2017-06-03 18:06:11 -0700

    Implemented Segmented Sieve algorithm and corresponding unit tests to reduce memory usage on calculating large arrays of prime numbers

--- SEApp/SieveMain.cs
+++ SEApp/SieveMain.cs
@@ -8,54 +8,106 @@ namespace SEApp
 {
     public class SieveMain
     {
-        //private double dResult;
-        //private string strResult;
-        //private SortedSet<int> m_generatedNums = new SortedSet<int>();
-
-        public List<int> ComputePrimes( int upperBound )
+        public List<int> ComputePrimesBasic( int upperBound )
         {
-            //int[] primeNums = new int[upperBound+1];
             List<int> primeNums = new List<int>();
-            int upperBoundSquareRoot = (int)Math.Sqrt( upperBound );
-
-            // Perfmance improvements; convert bool array to bit array
-            bool[] isNotPrimeNum = new bool[upperBound + 1];
-            //System.Collections.BitArray isNotPrimeNum = new System.Collections.BitArray(upperBound+1, false);
-
-            // We only need to go from values 2 ... sqrt(upperBound)
-            // TODO: Explain why
-            for( int i = 2; i <= upperBoundSquareRoot; i++ )
+            bool[] markedNums = new bool[upperBound+1];
+            
+            // Loop through all numbers starting from 2 to sqrt(n)
+            // Only need to go to sqrt(n) since all numbers after sqrt(n) will have been marked already
+            int upperBoundSquareRoot = (int)Math.Floor( Math.Sqrt( upperBound ) );
+            for( int i = 2; i < upperBoundSquareRoot; i++ )
             {
-                // If the prime number has yet to be marked TRUE
-                if( !isNotPrimeNum[i] )
+                // If this has yet to be marked as a non-prime number
+                if( !markedNums[i] )
                 {
-                    // Add the initial prime number to the list
-                    primeNums.Add( i );
-
-                    // Then, mark all multiples of i*j starting from i^2
+                    // Loop through all multiples of the prime number starting from the prime squared
+                    // All values from the prime number to the prime number squared will have been marked already
                     for( int j = i*i; j < upperBound; j += i )
                     {
-                        isNotPrimeNum[j] = true;
+                        // Mark all multiples of the prime since these aren't prime numbers
+                        markedNums[j] = true;
                     }
                 }
             }
 
-            // Now that all the non prime numbers are marked
-            // Iterate from sqrt(upperbound) to upperbound
-            // All false/unmarked numbers are primes 
-            for( int i = upperBoundSquareRoot; i <= upperBound; i++ )
+            // Loop through all marked numbers and add the numbers to the list that are still unmarked (prime numbers)
+            for( int i = 2; i < upperBound; i++ )
             {
-                if( !isNotPrimeNum[i] )
-                {
-                    // Add the prime numbers to the list
-                    //System.Diagnostics.Debug.Write( i );
+                if( !markedNums[i] )
                     primeNums.Add( i );
-                }
             }
-
             return primeNums;
-            //int[] generatedNums = Enumerable.Range( 2, upperBound ).ToArray<int>();
+        }
 
+        public List<int> ComputePrimesSegmented( int upperBound )
+        {
+            List<int> primeNums = new List<int>();
+            List<int> primeNumsBefore = new List<int>();
+
+            // Define a limit such that the numbers from 2 to upperBound can be segmented
+            // and the prime numbers can be calculated for each segment/
+            // The size of each segment is the limit value
+            int limit = (int)Math.Floor( Math.Sqrt( upperBound ) ) + 1;
+
+            // First compute all the prime numbers from 2 to sqrt(upperBound);
+            // i.e The first segment
+            primeNumsBefore = ComputePrimesBasic( limit );
+
+            // All the first segment of prime numbers to the list
+            primeNums.AddRange( primeNumsBefore );
+
+            // Define the lower and upper bounds for the next segment
+            // These bounds are updated for each segment
+            int lowerBoundSegment = limit;
+            int upperBoundSegment = 2*limit;
+
+            // Calculate prime numbers for each segment and stop when we get to the last segment
+            // i.e. last segment is defined where the lower bound of the last segment is still less than
+            // the input upperBound
+            while( lowerBoundSegment < upperBound )
+            {
+                // Create a boolean array the size of the segment + 1 to mark values that are prime numbers
+                bool[] markedNums = new bool[limit+1];
+
+                // Initialize all the values in the boolean array to true
+                for( int i = 0; i < markedNums.Length; i++ )
+                    markedNums[i] = true;
+
+                // Loop over all the prime numbers that have already been calculated (the first segment)
+                for( int i = 0; i < primeNumsBefore.Count(); i++ )
+                {
+                    // Start calculating the new primes at a lowerLimit value, 
+                    // where the lowerLimit is in current segment and is the lowest
+                    // number in the segment that is a multiple of the current prime number
+                    int lowerLimit = (int)Math.Floor( (double)lowerBoundSegment/primeNumsBefore[i] )*primeNumsBefore[i];
+                    if( lowerLimit < lowerBoundSegment )
+                        lowerLimit += primeNumsBefore[i];
+
+                    // Loop through the segment, from the lowerLimit/first multiple to the segment's upper bound
+                    // and set all multiples of the current prime number to false
+                    for( int j = lowerLimit; j < upperBoundSegment; j += primeNumsBefore[i] )
+                    {
+                        markedNums[j-lowerBoundSegment] = false;
+                    }
+                }
+
+                // For all values in this segment and markedNums that are true/prime numbers, add them to the return list
+                for( int i = lowerBoundSegment; i < upperBoundSegment; i++ )
+                {
+                    if( markedNums[i-lowerBoundSegment] )
+                        primeNums.Add( i );
+                }
+
+                // Set the lower and upper bounds of the current segment to the bounds of the new segment
+                lowerBoundSegment += limit;
+                upperBoundSegment += limit;
+
+                // If we've reached the last segment, the upper bound is now the input upperBound
+                if( upperBoundSegment >= upperBound )
+                    upperBoundSegment = upperBound;
+            }
+            return primeNums;
         }
 
     }
--- SEAppUnitTest/UnitTest.cs
+++ SEAppUnitTest/UnitTest.cs
@@ -6,31 +6,53 @@ using SEApp;
 
 namespace SEAppUnitTest
 {
+    // TODO: insert try/catch blocks for all tests
+    // for better descriptive error handling
     [TestClass]
     public class SieveMainTest
     {
         [TestMethod]
-        public void TestComputePrimes()
+        public void TestComputePrimesBasic()
         {
             // Prep test
             int upperBoundTestValue = 100;
             SieveMain sieve = new SEApp.SieveMain();
 
             // Execute test
-            sieve.ComputePrimes( upperBoundTestValue );
+            sieve.ComputePrimesBasic( upperBoundTestValue );
         }
 
         [TestMethod]
-        public void TestComputePrimesLargeInt()
+        public void TestComputePrimesSegmented()
         {
             // Prep test
-            int upperBoundTestValue = 2000000000;
+            int upperBoundTestValue = 100;
             SieveMain sieve = new SEApp.SieveMain();
 
             // Execute test
-            sieve.ComputePrimes( upperBoundTestValue );
+            sieve.ComputePrimesSegmented( upperBoundTestValue );
         }
 
+        [TestMethod]
+        public void TestComputePrimesBasicLargeInt()
+        {
+            // Prep test
+            int upperBoundTestValue = 1000000000;
+            SieveMain sieve = new SEApp.SieveMain();
 
+            // Execute test
+            sieve.ComputePrimesBasic( upperBoundTestValue );
+        }
+
+        [TestMethod]
+        public void TestComputePrimesSegmentedLargeInt()
+        {
+            // Prep test
+            int upperBoundTestValue = 1000000000;
+            SieveMain sieve = new SEApp.SieveMain();
+
+            // Execute test
+            sieve.ComputePrimesSegmented( upperBoundTestValue );
+        }
     }
 }
